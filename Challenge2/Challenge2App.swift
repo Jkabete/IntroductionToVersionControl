e2b7d676136c63e296bbc8295d751d9513a9def1
Jkabete <jacopo.abete30@hotmail.it>
2025-11-11 14:32:52 +0100

    initial commit

---
+++ Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by francesco jacopo abete on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
